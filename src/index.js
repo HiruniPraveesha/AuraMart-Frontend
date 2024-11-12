e0c20e697d4f3a48e539c6c555159907edbde245
Hiruni Praveesha <126430052+HiruniPraveesha@users.noreply.github.com>
2024-11-12 17:56:00 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
+import { ProductsContextProvider } from './context/ProductContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <BrowserRouter>
+        <React.StrictMode>
+            <AuthContextProvider>
+                    <ProductsContextProvider>
+                        <App />
+                    </ProductsContextProvider>
+            </AuthContextProvider>
+        </React.StrictMode>
+    </BrowserRouter>
+
+);
